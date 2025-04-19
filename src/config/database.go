d60ad13b7d0df21fffa3b675b59c136cab0942e7
vinimostaco <vini.mostaco@hotmail.com>
2025-04-18 22:56:44 -0300

    feat: add db config connection

---
+++ src/config/database.go
@@ -0,0 +1,39 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+	
+var DB *gorm.DB
+
+func Connect() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		host, user, password, dbname, port,
+	)
+
+	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+
+	fmt.Println("Database connected!")
+	DB = database
+}
--- src/main.go
+++ src/main.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/vinimostaco/open-finance/src/config"
 	"github.com/vinimostaco/open-finance/src/routes"
 )
 
 func main() {
+	config.Connect()
 	routes.SetupRoutes()
 
 	fmt.Println("🚀 Starting server on :8080")
